b8075a4eea0a3470dc38890d51bd1f034a4005dd
mohit <mohitpardeshi729@gmail.com>
2022-12-16 18:56:52 +0530

    time function created , runs every 1 sec

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 
-let time = new Date;
-console.log(time.getTime);
+
+
+function updateTime(){
+
+    let time = new Date();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    let timeText = document.getElementById("time")
+
+    timeText.innerHTML=hours+" "+minutes+" "+seconds;
+
+    console.log("function is running");
+    
+}
+
+setInterval(() => {
+    updateTime()
+}, 1000);
+
+
+
+
+
